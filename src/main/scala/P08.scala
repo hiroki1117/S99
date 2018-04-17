71b37fa26d8a854048aa59ea8505b5dedaa566be
raina <scala@gmail.com>
2018-04-17 15:42:09 +0900

    add P08

---
+++ src/main/scala/P08.scala
@@ -0,0 +1,13 @@
+
+object P08 {
+
+	def compress[T](list: List[T]): List[T] = {
+		list.foldRight(Nil: List[T]){(x, acc) =>
+			acc match {
+				case y :: ys if y == x => y :: ys
+				case c => x :: c
+			}
+		}
+	}
+
+}
