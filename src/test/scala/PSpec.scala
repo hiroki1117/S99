c8043d9ce955c2275786f6767152b87519cdf039
raina <scala@gmail.com>
2018-04-17 15:42:41 +0900

    add test

---
+++ src/test/scala/PSpec.scala
@@ -0,0 +1,13 @@
+
+import org.scalatest._
+import Matchers._
+
+class PSpec extends FlatSpec {
+
+	val list = (1 to 20).toList
+	
+	"P01" should "find the last" in {
+		P01.last(list) should be (20)
+	}
+
+}
