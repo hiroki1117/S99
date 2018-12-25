30ca179424b8ef101ccf963b033ed9e2ddc5508d
hiroki <scala1117@gmail.com>
2018-12-25 15:42:17 +0900

    P07からP12までのテストを追加

--- src/test/scala/hiroki1117/s99/PSpec.scala
+++ src/test/scala/hiroki1117/s99/PSpec.scala
@@ -6,6 +6,8 @@ import Matchers._
 class PSpec extends FlatSpec {
 
 	val list = (1 to 20).toList
+	val duplicateList = List(1,1,1,2,2,3,4,5,5)
+
 	
 	"P01" should "find the last" in {
 		P01.last(list) should be (20)
@@ -41,5 +43,30 @@ class PSpec extends FlatSpec {
 
 	it should "be false" in {
 		P06.isPalindrome(list) should be (false)
+	}
+
+	"P07" should "flat List" in {
+		P07.flatten(List(List(3,3), 6, List(9, List(7, 8)))) should be (List(3,3,6,9,7,8))
+	}
+
+	"P08" should "replaced repeated elements to single" in {
+		P08.compress(List(1,1,1,2,3,4,5,5,5,1)) should be (List(1,2,3,4,5,1))
+	}
+
+	"P09" should "make sublists from duplicates of list elements" in {
+		P09.pack(duplicateList) should be (List(List(1,1,1),List(2,2),List(3),List(4),List(5,5)))
+	}
+
+	"P10" should "turn duplicates list into tuple (Num, Element)" in {
+		P10.encode(duplicateList) should be (List((3,1), (2,2), (1,3), (1,4), (2,5)))
+	}
+
+	"P11" should "copy simpliy ellement if it is no duplicates. Only elements with duplicates are transferred as (N,E)" in {
+		P11.encodeModified(duplicateList) should be (List((3,1), (2,2), 3, 4, (2,5)))
+	}
+
+	"P12" should "decode a run-length encoded list generated as problem P10" in {
+		val encodedList = P10.encode(duplicateList)
+		P12.decode(encodedList) should be (duplicateList)
 	}
 }
