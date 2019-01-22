8d7944606d6c1716df1d22b26625dd2ca050df15
hiroki <scala1117@gmail.com>
2019-01-22 17:03:13 +0900

    パッケージオブジェクトの宣言の修正

--- src/main/scala/hiroki1117/s99/package.scala
+++ src/main/scala/hiroki1117/s99/package.scala
@@ -1,4 +1,4 @@
-package hiroki1117.s99
+package hiroki1117
 
 import scala.collection.mutable.ListBuffer
 
--- src/test/scala/hiroki1117/s99/ArithmeticSpec.scala
+++ src/test/scala/hiroki1117/s99/ArithmeticSpec.scala
@@ -4,7 +4,6 @@ import org.scalatest._
 import Matchers._
 
 class ArithmeticSpec extends FlatSpec {
-  import s99._
 
   "P31" should "determine whether a given integer number is prime" in {
     7.isPrime should be (true)
