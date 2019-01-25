61298747cbc1a1fe5a8f25d0ce8caea992682b6f
hiroki <scala1117@gmail.com>
2019-01-25 11:48:01 +0900

    P27(a)(b)の呼び出しに対してListの長さをチェックする

--- src/main/scala/hiroki1117/s99/P27.scala
+++ src/main/scala/hiroki1117/s99/P27.scala
@@ -2,14 +2,18 @@ package hiroki1117.s99
 
 object P27 {
   def group3[A](list: List[A]): List[List[List[A]]] = {
+    require(list.length==9, "List length should be 9")
     permutation(list).map(x => List(x.take(2), x.take(5).drop(2), x.drop(5)))
   }
 
   def group3_[A](list: List[A]): List[List[List[A]]] = {
+    require(list.length==9, "List length should be 9")
     group(List(2,3,4), list)
   }
 
   def group[A](split: List[Int], list: List[A]): List[List[List[A]]] = {
+    require(split.length==3, "First List length should be 3")
+    require(list.length==9, "Second List length should be 9")
     val a::b::_ = split
     permutation(list).map(x => List(x.take(a), x.take(a+b).drop(a), x.drop(a+b)))
   }
